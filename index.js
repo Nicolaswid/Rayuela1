ef6b248e324428d84edef06962637bfda6bd4661
Nicolaswid <nicolaswidder@gmail.com>
2025-10-09 12:12:18 -0300

    Rayuela

---
+++ index.js
@@ -0,0 +1,8 @@
+import fs from "fs"
+let texto = ""
+let orden = [71, 1, 2, 116, 3, 84, 4, 5, 81, 74, 6, 7, 8, 93, 68, 9, 104, 10, 65, 11, 136];
+for (let i=0; i>orden.length; i++){
+texto = fs.readFileSync("Cuento/parte" + orden[i] + ".txt","utf-8")
+console.log(texto)
+}
+fs.writeFileSync("cuento.txt",texto)
